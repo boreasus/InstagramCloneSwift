6e459818b78c1d361b494abb671bedcaa497a86d
boreasus <>
2023-01-10 14:53:20 +0300

    First

---
+++ instagramClone/Controllers/CoreTabs/ExploreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreViewController.swift
+//  instagramClone
+//
+//  Created by poyraz on 10.01.2023.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ instagramClone/Controllers/CoreTabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagramClone
+//
+//  Created by poyraz on 10.01.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
